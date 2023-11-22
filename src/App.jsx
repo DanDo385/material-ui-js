25a3c900f34bad244f47cf7a79b4402957d981d5
Daniel Magro <dando385@yahoo.com>
2023-11-22 17:19:29 -0500

    added cssbaseline and formattted homepage

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Typography, AppBar, Card, CardActions, CardMedia, CssBaseline, Grid, ToolBar, Container } from "@mui/material"
+import { PhotoCamera } from "@mui/icons-material"
+
+const App = () => {
+  return (
+    <>
+        <CssBaseline />
+        <AppBar position="relative">
+            <ToolBar>
+                <PhotoCamera />
+                <Typography variant="h6">
+                    Photo Album
+                </Typography>
+            </ToolBar>
+
+    </>
+  )
+
+
+export default App
