fa7f91564ec7e84288b96232010412c8759aaa5e
Daniel Magro <dando385@yahoo.com>
2023-11-26 11:22:08 -0500

    added more typography

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Typography, AppBar, Card, CardActions, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@mui/material"
-import { PhotoCamera } from "@mui/icons-material"
+import { Typography, AppBar, Card, CardActions, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material'
+import { PhotoCamera } from '@mui/icons-material'
 
 const App = () => {
   return (
@@ -11,6 +11,19 @@ const App = () => {
                 <Typography variant="h6">Photo Album</Typography>
             </Toolbar>
         </AppBar>
+
+        <main>
+            <div>
+                <Container maxWidth="sm">
+                    <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                        Photo Album
+                    </Typography>
+                    <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                        Hello everyone this is a photo album and I'm trying to make this sentence as long as possible so we can see how it looks like on the screen
+                    </Typography>
+                </Container>
+            </div>
+        </main>
     </>
   )
 }
