9de8a6685414eb6fb43bb2ae1b76ef12e1f2965d
Daniel Magro <dando385@yahoo.com>
2023-11-29 22:29:25 -0500

    updated as per yt video

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
-import { Typography, AppBar, Card, CardActions, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material'
-import { PhotoCamera } from '@mui/icons-material'
+import { Typography, AppBar, Card, CardActions, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material';
+import { PhotoCamera } from '@mui/icons-material';
 
 const App = () => {
   return (
         <>
-        <CssBaseline />
-        <AppBar position="relative">
+            <CssBaseline />
+            <AppBar position="relative">
             <Toolbar>
                 <PhotoCamera />
                 <Typography variant="h6">Photo Album</Typography>
             </Toolbar>
-        </AppBar>
+            </AppBar>
 
-        <main>
-            <div>
-                <Container maxWidth="sm">
-                    <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+            <main>
+                <div>
+                    <Container maxWidth="sm">
+                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
                         Photo Album
-                    </Typography>
-                    <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                        </Typography>
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
                         Hello everyone this is a photo album and I'm trying to make this sentence as long as possible so we can see how it looks like on the screen
-                    </Typography>
-                </Container>
-            </div>
-        </main>
-    </>
+                        </Typography>
+                    </Container>
+                </div>
+             </main>
+        </>
   )
 }
 
-export default App
+export default App;
